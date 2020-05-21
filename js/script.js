d2ffc06ed2ef1e2db3c0fd497853892dd812be4e
Aldo Cahuana <aldooow@icloud.com>
2020-05-21 18:18:59 +0200

    Work in progress!!!!!!

--- js/script.js
+++ js/script.js
@@ -21,18 +21,26 @@ var finestraVisible = document.getElementById('biglietto');
 // Click-Genera!!!
 buttonGenera.addEventListener('click',
   function () {
+    // Far apparire il Biglietto quando si fa click in "Genera"!!!!
     finestraVisible.className = 'visible';
-
+    // Nome stampato sul Biglietto!!!
     biglNomeCognome.innerHTML = formNomeCognome.value;
-    biglOfferta.innerHTML = 'Prezzo Intero'
+    // Offerta stampata sul Biglietto!!!
+    biglOfferta.innerHTML = 'Prezzo Intero';
+    // Numero di Carrozza stampata sul Biglietto!!!!
     biglCarrozza.innerHTML = ((Math.random()) * 100).toFixed(0);
+    // Codice CP (6 cifre) stampato sul Biglietto!!!
     biglCodiceCp.innerHTML = ((Math.random()) * 1000000).toFixed(0);
-    biglCostoBiglietto.innerHTML = 0.21 * formKilometri.value;
+    // Costo stampato sul Biglietto!!!
+    biglCostoBiglietto.innerHTML = '€' + (0.21 * formKilometri.value).toFixed(2);
+
     if (formFasciaEta.value == 'Minorenne') {
-      biglCostoBiglietto.innerHTML = 0.21 * formKilometri.value * 80 / 100;
+      // Sconto Minorenne!!
+      biglCostoBiglietto.innerHTML = '€' + (0.21 * formKilometri.value * 80 / 100).toFixed(2);
       biglOfferta.innerHTML = 'Prezzo Ridotto Minorenne';
     } else if (formFasciaEta.value == 'Over 65'){
-      biglCostoBiglietto.innerHTML = 0.21 * formKilometri.value * 60 / 100;
+      // Sconto Over65!!
+      biglCostoBiglietto.innerHTML = '€' + (0.21 * formKilometri.value * 60 / 100).toFixed(2);
       biglOfferta.innerHTML = 'Prezzo Ridotto Over 65'
     }
   }
@@ -42,6 +50,7 @@ buttonGenera.addEventListener('click',
 buttonAnnulla.addEventListener('click',
   function () {
 
+    // Far Nascondere il Biglietto quando si fa click in "Annulla"!!!!
     finestraVisible.className = 'hidden';
 
     formNomeCognome.value = '';
