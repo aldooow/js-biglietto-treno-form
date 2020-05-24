91b15e39d2d03694e4f594074c4cb3adb1663570
Aldo Cahuana <aldooow@icloud.com>
2020-05-24 21:30:07 +0200

    Completed!!!

--- js/script.js
+++ js/script.js
@@ -1,71 +1,74 @@
+// Variabili dei Buttons!!!
+var buttonGenera = document.getElementById('js_button-genera');
+var buttonAnnulla = document.getElementById('js_button-annulla');
+// Variabili del form!!!
+var formNome = document.getElementById('js_form-nome');
+var formKilometri = document.getElementById('js_form-kilometri');
+var formEta = document.getElementById('js_form-eta');
+//Variabili del ticket stampato!!!
+var ticketNome = document.getElementById('js_ticket-nome');
+var ticketOfferta = document.getElementById('js_ticket-offerta');
+var ticketCosto = document.getElementById('js_ticket-costo');
+var ticketCarrozza = document.getElementById('js_ticket-carrozza');
+var ticketCodice = document.getElementById('js_ticket-codice');
+// Variante per nascondere il Ticket!!!
+var hiddenTicket = document.getElementById('js_hidden-ticket');
+var hiddenError = document.getElementById('js_hidden-error');
 
-// Buttons!!!!
-var buttonGenera = document.getElementById('button-genera');
-var buttonAnnulla = document.getElementById('button-annulla');
-
-// Dati del Form - Da compilare dal Utente!!!!
-var formNomeCognome = document.getElementById('form-nome-cognome');
-var formKilometri = document.getElementById('form-kilometri');
-var formFasciaEta = document.getElementById('form-fascia-eta');
-
-
-// Dati del Biglietto Compilato!!!
-var biglNomeCognome = document.getElementById('bigl-nome-cognome');
-var biglCostoBiglietto = document.getElementById('bigl-costo-biglietto');
-var biglOfferta = document.getElementById('bigl-offerta');
-var biglCarrozza = document.getElementById('bigl-carrozza');
-var biglCodiceCp = document.getElementById('bigl-codice-cp');
-
-// Variante del Biglietto
-var bigliettoVisible = document.getElementById('biglietto');
-var titoloVisible = document.getElementById('titolo');
-
-
-// Click-Genera!!!
+// Eventi dopo il "Click" sul bottone "GENERA"!!!!
 buttonGenera.addEventListener('click',
-  function () {
-    // Far apparire il Biglietto quando si fa click in "Genera"!!!!
-    bigliettoVisible.className ='wrapper';
-    titoloVisible.className = '';
-    // Nome stampato sul Biglietto!!!
-    biglNomeCognome.innerHTML = formNomeCognome.value;
-    // Offerta stampata sul Biglietto!!!
-    biglOfferta.innerHTML = 'Prezzo Intero';
-    // Numero di Carrozza stampata sul Biglietto!!!!
-    biglCarrozza.innerHTML = ((Math.random()) * 100).toFixed(0);
-    // Codice CP (6 cifre) stampato sul Biglietto!!!
-    biglCodiceCp.innerHTML = ((Math.random()) * 1000000).toFixed(0);
-    // Costo stampato sul Biglietto!!!
-    biglCostoBiglietto.innerHTML = '€' + (0.21 * formKilometri.value).toFixed(2);
+ function() {
+  // Validazione Nome e KM inseriti (QUANDO NON INSERISCONO IL NOME O I KILOMETRI CHE DEVONO PERCORRER, VIENE VISSUALIZZATO UN MESSAGGIO DI ERROR)!!!!
+  if ((formNome.value == '') || (formNome.value == 'undefined')) {
+    alert("Il campo Nome è obbligatorio.");
+    hiddenTicket.className = 'hidden';
+    hiddenError.className = 'error-wrapper';
+  } else if (formKilometri.value == 0) {
+    alert("Il campo non puo essere 0.");
+    hiddenTicket.className = 'hidden';
+    hiddenError.className = 'error-wrapper';
+  } else {
 
-    if (formFasciaEta.value == 'Minorenne') {
-      // Sconto Minorenne!!
-      biglCostoBiglietto.innerHTML = '€' + (0.21 * formKilometri.value * 80 / 100).toFixed(2);
-      biglOfferta.innerHTML = 'Prezzo Ridotto Minorenne';
-    } else if (formFasciaEta.value == 'Over 65'){
-      // Sconto Over 65!!
-      biglCostoBiglietto.innerHTML = '€' + (0.21 * formKilometri.value * 60 / 100).toFixed(2);
-      biglOfferta.innerHTML = 'Prezzo Ridotto Over 65'
+    //FAR APPARIRE IL TICKET!!
+    hiddenTicket.className = 'ticket-printed';
+
+    // Stampare il nome sul ticket!!
+    ticketNome.innerHTML = formNome.value;
+    // Stampare numero di Carrozza!!
+    ticketCarrozza.innerHTML = '<b>A</b>' + (((Math.random()) * 10) + 1).toFixed(0);
+    ticketCodice.innerHTML = '<b>CP</b>' + (((Math.random()) * 1000) + 1).toFixed(0);
+    // Stampare il costo del Biglietto e il tipo di Offerta (Tenendo in conto gli sconti)!!!
+    ticketCosto.innerHTML = (formKilometri.value * 0.21).toFixed(2) + '<b>Eur</b>';
+    ticketOfferta.innerHTML = 'Prezzo Intero';
+      // Offerta Minorenne del 20% di sconto e Over65 del 40% di sconto!!
+    if (formEta.value == 'minorenne') {
+      ticketCosto.innerHTML = ((formKilometri.value * 0.21) * 80 / 100).toFixed(2) + '<b>Eur</b>';
+      ticketOfferta.innerHTML = 'Prezzo Minorenne';
+    } else if (formEta.value == 'over65') {
+      ticketCosto.innerHTML = ((formKilometri.value * 0.21) * 60 / 100).toFixed(2) + '<b>Eur</b>';
+      ticketOfferta.innerHTML = 'Prezzo Over65';
     }
-  }
+
+  };
+
+ }
 );
 
-// Click-Annulla!!!
+// Eventi dopo il "Click" sul bottone "ANNULLA"!!!!
 buttonAnnulla.addEventListener('click',
-  function () {
-
-    // Far Nascondere il Biglietto quando si fa click in "Annulla"!!!!
-    bigliettoVisible.className ='hidden';
-    titoloVisible.className = 'hidden';
-
-    formNomeCognome.value = '';
-    formKilometri.value = '';
-    formFasciaEta.value = 'Maggiorenne';
-
-    biglNomeCognome.innerHTML = '';
-    biglOfferta.innerHTML = '';
-    biglCarrozza.innerHTML = '';
-    biglCodiceCp.innerHTML = '';
-    biglCostoBiglietto.innerHTML = '';
-  }
-)
+ function () {
+   // Nascondere Ticket!!!
+  hiddenTicket.className = 'ticket-printed hidden';
+  hiddenError.className = 'error-wrapper hidden';
+  // Reset del form!!!
+  formNome.value = '';
+  formKilometri.value = '';
+  formEta.value = 'maggiorenne';
+  // Reset del Ticket!!!
+  ticketCarrozza.innerHTML = '';
+  ticketCodice.innerHTML = '';
+  ticketNome.innerHTML = '';
+  ticketCosto.innerHTML = '';
+  ticketOfferta.innerHTML = '';
+ }
+);
